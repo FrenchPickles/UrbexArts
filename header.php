a5d6814a487273234d0dcd1a6b51a52be28ce387
steven <steven.douillet@gmail.com>
2018-12-27 16:57:05 +0100

    Add title and favicon

--- header.php
+++ header.php
@@ -7,12 +7,13 @@ session_start();
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
+	<link rel="icon" type="image/png" href="assets/images/favicon-logo-urbexarts.png">
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<title></title>
+	<title>UrbexArts | Bâtiments abandonnés et souterrains !</title>
 </head>
 
 <body>
